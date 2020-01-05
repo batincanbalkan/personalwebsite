133fbeeb46259d5bc67ecfc5ec7ff32ae4d2be57
Batin Can Balkan <batin.balkan@mail.mcgill.ca>
2020-01-05 07:58:43 -0500

    added chatbox

---
+++ contactus.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $("#on").click(function(){
+    if ( $("map").css('visibility') = 'hidden') {
+      $("map").css('visibility') = 'visible';
+    }
+    else {
+      $("map").css('visibility') = 'hidden';
+    }
+  });
+
+});
