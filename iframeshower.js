f895ca9584670afd89e20fffff0a7466f7793007
Batin Can Balkan <batin.balkan@mail.mcgill.ca>
2020-01-06 09:44:53 -0500

    now iframe is hidden until the button is clicked

---
+++ iframeshower.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#on').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#map').html('<iframe src="https://fxo.io/m/58226ew6" width="100%" height="100%" style="border: 3px solid rgb(0, 204, 204); width: 100%; height: 100%;" allowfullscreen></iframe>');
+        }
+    });   
+});
